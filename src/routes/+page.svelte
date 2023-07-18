a8f906544c4276ca0efbd9186498abe79309f20c
diyakolhe <kolhedi61@craighead.school.nz>
2023-07-19 10:50:01 +1200

    Finished game
    This is the finished game... improvements are being made

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,82 @@
 <script>
-  import Header from '$lib/Header.svelte'
+  import Header from "$lib/Header.svelte"
+  let src = ""
+  let player1
+  let player2
+  let result = ""
+
+  function paper() {
+    src = "/paper 3.png"
+    player1 = "paper"
+    player2 = ""
+  }
+
+  function scissors() {
+    src = "/scissors 3.png"
+    player1 = "scissors"
+    player2 = ""
+  }
+
+  function rock() {
+    src = "/rock 2.png"
+    player1 = "rock"
+    player2 = ""
+  }
+
+  function play() {
+    let choices = ["/paper 3.png", "/scissors 3.png", "/rock 2.png"]
+    let index = Math.floor(Math.random(0) * choices.length)
+    player2 = choices[index]
+  }
 </script>
 
 <Header />
 
 <main>
-  <h2>SvelteKit</h2>
+  <h2>Paper, Scissors, Rock</h2>
 
-  <p>Welcome to coding with SvelteKit, a modern JavaScript framework that makes it easy to code great apps.</p>
+  <p>Choose one:</p>
+  <button on:click={paper}><img src="/paper 3.png" alt="" /></button>
+  <button on:click={scissors}><img src="/scissors 3.png" alt="" /></button>
+  <button on:click={rock}><img src="rock 2.png" alt="" /></button>
+
+  <p>Your choice:</p>
+  <img {src} alt="" /><br />
+  <button on:click={play}>Generate Opponents choice</button>
+  <p>Opponents Choice:</p>
+  <img src={player2} alt="" />
+
+  {#if player1 == "paper" && player2 == "/paper 3.png"}
+    <p>Tie</p>
+  {:else if player1 == "paper" && player2 == "/scissors 3.png"}
+    <p>You lost</p>
+  {:else if player1 == "paper" && player2 == "/rock 2.png"}
+    <p>You won</p>
+  {/if}
+
+  {#if player1 == "scissors" && player2 == "/paper 3.png"}
+    <p>You won</p>
+  {:else if player1 == "scissors" && player2 == "/scissors 3.png"}
+    <p>Tie</p>
+  {:else if player1 == "scissors" && player2 == "/rock 2.png"}
+    <p>You lost</p>
+  {/if}
+
+  {#if player1 == "rock" && player2 == "/paper 3.png"}
+    <p>You lost</p>
+  {:else if player1 == "rock" && player2 == "/scissors 3.png"}
+    <p>You won</p>
+  {:else if player1 == "rock" && player2 == "/rock 2.png"}
+    <p>Tie</p>
+  {/if}
 </main>
 
 <footer>
   <p>&copy; Craighead Diocesan School 2023</p>
 </footer>
+
+<style>
+  img {
+    width: 10em;
+  }
+</style>
---
+++ src/routes/chatGPT/+page.svelte
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Rock Paper Scissors</title>
+    <style>
+      body {
+        text-align: center;
+        font-family: Arial, sans-serif;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Rock Paper Scissors</h1>
+    <p>Select your move:</p>
+    <button onclick="play('rock')">Rock</button>
+    <button onclick="play('paper')">Paper</button>
+    <button onclick="play('scissors')">Scissors</button>
+    <p id="result" />
+
+    <script>
+      function play(userChoice) {
+        // Generate computer's move
+        const moves = ["rock", "paper", "scissors"]
+        const computerChoice = moves[Math.floor(Math.random() * moves.length)]
+
+        // Determine the winner
+        let result = ""
+        if (userChoice === computerChoice) {
+          result = "It's a tie!"
+        } else if ((userChoice === "rock" && computerChoice === "scissors") || (userChoice === "paper" && computerChoice === "rock") || (userChoice === "scissors" && computerChoice === "paper")) {
+          result = "You win!"
+        } else {
+          result = "Computer wins!"
+        }
+
+        // Display the result
+        document.getElementById("result").innerHTML = `You chose ${userChoice}. The computer chose ${computerChoice}. ${result}`
+      }
+    </script>
+  </body>
+</html>
